a985e2bcc456e568a231c2ff6e45d52f26c15f36
Kathy Hang <knhang07@gmail.com>
2024-09-16 20:47:33 -0700

    Displayed grid, no mines added or stopwatch. 1st commit!

---
+++ app/src/main/java/com/example/project1_minesweeper/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.project1_minesweeper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.GridLayout;
+
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int COLUMN_COUNT = 10;
+    private static final int ROW_COUNT = 12;
+
+    // save the TextViews of all cells in an array, so later on,
+    // when a TextView is clicked, we know which cell it is
+    private ArrayList<TextView> cell_tvs;
+
+    private int dpToPixel(int dp) {
+        float density = Resources.getSystem().getDisplayMetrics().density;
+        return Math.round(dp * density);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cell_tvs = new ArrayList<TextView>();
+
+        GridLayout grid = (GridLayout) findViewById(R.id.gridLayout01);
+        grid.setColumnCount(COLUMN_COUNT);
+        grid.setRowCount(ROW_COUNT);
+        for (int i = 0; i<12; i++) {
+            for (int j= 0; j<10; j++) {
+                TextView tv = new TextView(this);
+                tv.setHeight( dpToPixel(32) );
+                tv.setWidth( dpToPixel(32) );
+                tv.setTextSize( 32 );//dpToPixel(32) );
+                tv.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
+                tv.setTextColor(Color.GRAY);
+                tv.setBackgroundColor(Color.GRAY);
+                tv.setOnClickListener(this::onClickTV);
+
+                GridLayout.LayoutParams lp = new GridLayout.LayoutParams();
+                lp.setMargins(dpToPixel(2), dpToPixel(2), dpToPixel(2), dpToPixel(2));
+                lp.rowSpec = GridLayout.spec(i);
+                lp.columnSpec = GridLayout.spec(j);
+
+                grid.addView(tv, lp);
+
+                cell_tvs.add(tv);
+            }
+        }
+
+        placeMines();
+    }
+
+    // Keep this
+    private int findIndexOfCellTextView(TextView tv) {
+        for (int n=0; n<cell_tvs.size(); n++) {
+            if (cell_tvs.get(n) == tv)
+                return n;
+        }
+        return -1;
+    }
+
+    private void placeMines(){
+        Random random = new Random();
+
+        for (int i = 0; i < 4; i++){
+            int row = random.nextInt(ROW_COUNT);
+            int col = random.nextInt(COLUMN_COUNT);
+        }
+    }
+
+    // Function that displays the bomb, or # of adj bombs nearby. Uses a listener
+    public void onClickTV(View view){
+        TextView tv = (TextView) view;
+        int n = findIndexOfCellTextView(tv);
+        int i = n/COLUMN_COUNT;
+        int j = n%COLUMN_COUNT;
+        tv.setText(String.valueOf(i)+String.valueOf(j));
+        if (tv.getCurrentTextColor() == Color.GRAY) {
+            tv.setTextColor(Color.GREEN);
+            tv.setBackgroundColor(Color.parseColor("lime"));
+        }else {
+            tv.setTextColor(Color.GRAY);
+            tv.setBackgroundColor(Color.LTGRAY);
+        }
+    }
+}
