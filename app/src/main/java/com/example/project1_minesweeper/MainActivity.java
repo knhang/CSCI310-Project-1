455a84aa2ce69d37daa98b1820ecf926edb77c1b
Kathy Hang <knhang07@gmail.com>
2024-09-17 23:52:40 -0700

    Implemented mines and the functions to count how many mines are in adjacent cells + revision for onClick

--- app/src/main/java/com/example/project1_minesweeper/MainActivity.java
+++ app/src/main/java/com/example/project1_minesweeper/MainActivity.java
@@ -19,7 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
     // save the TextViews of all cells in an array, so later on,
     // when a TextView is clicked, we know which cell it is
-    private ArrayList<TextView> cell_tvs;
+    private ArrayList<TextView> cell_tvs; // UI array
+    private int[][] gridState; // Handles all the logical, backend 2D array
 
     private int dpToPixel(int dp) {
         float density = Resources.getSystem().getDisplayMetrics().density;
@@ -32,16 +33,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         cell_tvs = new ArrayList<TextView>();
+        gridState = new int[ROW_COUNT][COLUMN_COUNT];
 
         GridLayout grid = (GridLayout) findViewById(R.id.gridLayout01);
         grid.setColumnCount(COLUMN_COUNT);
         grid.setRowCount(ROW_COUNT);
-        for (int i = 0; i<12; i++) {
-            for (int j= 0; j<10; j++) {
+        for (int i = 0; i<ROW_COUNT; i++) {
+            for (int j= 0; j< COLUMN_COUNT; j++) {
                 TextView tv = new TextView(this);
                 tv.setHeight( dpToPixel(32) );
                 tv.setWidth( dpToPixel(32) );
-                tv.setTextSize( 32 );//dpToPixel(32) );
+                tv.setTextSize( 16 );//dpToPixel(32) );
                 tv.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
                 tv.setTextColor(Color.GRAY);
                 tv.setBackgroundColor(Color.GRAY);
@@ -70,28 +72,107 @@ public class MainActivity extends AppCompatActivity {
         return -1;
     }
 
+    // Place 4 random mines when we create the grid and keep track in gridState
     private void placeMines(){
         Random random = new Random();
-
         for (int i = 0; i < 4; i++){
             int row = random.nextInt(ROW_COUNT);
             int col = random.nextInt(COLUMN_COUNT);
+            if (gridState[row][col] != -1){
+                gridState[row][col] = -1; // -1 indicates there's a bomb
+            }
         }
+
+        // Go through gridState
+        for (int i = 0; i < ROW_COUNT; i++) {
+            for (int j = 0; j < COLUMN_COUNT; j++) {
+                // Skip mines
+                if (gridState[i][j] == -1) {
+                    continue;
+                }
+
+                // Update this cell w/ how many adjacent mines there are
+                gridState[i][j] = countMines(i, j);
+            }
+        }
+    }
+
+    private int countMines(int row, int col){
+        // find the mines first and then use the direction index to find
+        int[][] directions = {
+                {-1, 0},   // up
+                {1, 0},    // down
+                {0, -1},   // left
+                {0, 1},    // right
+                {-1, -1},  // up-left diagonal
+                {-1, 1},   // up-right diagonal
+                {1, -1},   // down-left diagonal
+                {1, 1}     // down-right diagonal
+        };
+        int mines = 0;
+
+        for (int[] dir : directions){
+            int newRow = row + dir[0];
+            int newCol = col + dir[1];
+
+            if (newRow >= 0 && newRow < ROW_COUNT && newCol >= 0 && newCol < COLUMN_COUNT){
+                if (gridState[newRow][newCol] == -1){
+                    mines++;
+                }
+            }
+        }
+
+        return mines;
     }
 
     // Function that displays the bomb, or # of adj bombs nearby. Uses a listener
     public void onClickTV(View view){
         TextView tv = (TextView) view;
         int n = findIndexOfCellTextView(tv);
-        int i = n/COLUMN_COUNT;
-        int j = n%COLUMN_COUNT;
-        tv.setText(String.valueOf(i)+String.valueOf(j));
-        if (tv.getCurrentTextColor() == Color.GRAY) {
-            tv.setTextColor(Color.GREEN);
-            tv.setBackgroundColor(Color.parseColor("lime"));
-        }else {
-            tv.setTextColor(Color.GRAY);
-            tv.setBackgroundColor(Color.LTGRAY);
+        int i = n / ROW_COUNT;
+        int j = n % COLUMN_COUNT;
+
+        // Display bomb
+        if (gridState[i][j] == -1){
+            tv.setText(R.string.mine);
+            // Handle game over and result activity page
+        }
+        else if (gridState[i][j] > 0){ // If its just a normal grid
+            tv.setText(String.valueOf(gridState[i][j]));
+        }
+        else{
+            // Display adjacent cells!
+            tv.setText("");
+            displayAdjCells(i, j);
+            // Hello.
+        }
+
+    }
+
+    private void displayAdjCells(int row, int col){
+        int[][] directions = {
+                {-1, 0},   // up
+                {1, 0},    // down
+                {0, -1},   // left
+                {0, 1},    // right
+                {-1, -1},  // up-left diagonal
+                {-1, 1},   // up-right diagonal
+                {1, -1},   // down-left diagonal
+                {1, 1}     // down-right diagonal
+        };
+
+        for (int[] dir : directions){
+            int newRow = row + dir[0];
+            int newCol = col + dir[1];
+
+            if (newRow >= 0 && newRow < ROW_COUNT && newCol >= 0 && newCol < COLUMN_COUNT) {
+                int n = newRow * ROW_COUNT + newCol;
+
+                TextView tv = cell_tvs.get(n);
+                tv.setText("");
+            }
         }
     }
+
+
 }
